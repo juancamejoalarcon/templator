2182db7c1f75aa3c10a92dc2e64e3f9559a6f475
Juan Camejo Alarcón <juancamejo93@gmail.com>
2024-02-04 19:55:58 +0100

    wip

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+    base: "./",
     resolve: {
         alias: {
             '@': path.resolve(__dirname, './src'),
